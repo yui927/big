b6afd1f7167c6582f8853b508b581cbd858becd8
yui <1364941759@qq.com>
2020-09-10 16:13:30 +0800

    登录和注册功能优化完善

--- assets/js/login.js
+++ assets/js/login.js
@@ -74,6 +74,7 @@
 // })
 
 $(function () {
+  // 点击事件显示隐藏登录注册表单
   $("#link-login").on("click", function () {
     $(".login-box").hide()
     $(".reg-box").show()
@@ -82,6 +83,7 @@ $(function () {
     $(".login-box").show()
     $(".reg-box").hide()
   })
+
   var form = layui.form;
   // 通过 form.verify() 函数自定义校验规则
   form.verify({
@@ -92,6 +94,50 @@ $(function () {
         return "两次密码输入不一致"
       }
     }
+  })
 
+  var layer = layui.layer
+  // 给注册表单绑定提交事件
+  $("#form-reg").on("submit", function (e) {
+    //阻止表单默认行为
+    e.preventDefault()
+    $.post("/api/reguser",
+      {
+        username: $("#form-reg [name=username]").val(),
+        password: $("#form-reg [name=password]").val()
+      },
+      function (res) {
+        if (res.status !== 0) {
+          return layer.msg(res.message);
+        }
+        layer.msg("注册成功,请登录!")
+        //模拟人的点击行为,去登录
+        $("#link-reg").click()
+
+      }
+    )
+  })
+
+  //建听登陆表单的提交事件
+  $("#form-login").on("submit", function (e) {
+    e.preventDefault()
+    $.ajax({
+      url: "/api/login",
+      //jquery快速获取表单数据
+      data: $(this).serialize(),
+      method: 'post',
+      success: function (res) {
+        if (res.status !== 0) {
+          return layer.msg("登录失败!")
+        }
+        layer.msg("登录成功!")
+        //将登录成功得到的token字符串,保存到localStorage中
+        localStorage.setItem("token",res.token)
+        // console.log(res.token)
+        //跳转到后台主页
+        location.href = "/index.html"
+      }
+
+    })
   })
 })
